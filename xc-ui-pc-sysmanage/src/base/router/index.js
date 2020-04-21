dc2edd0480951cbc6d987b63eafe07afb362f248
IBXiaomi <13572226807@163.com>
2020-04-21 23:54:30 +0800

    完成cms页面查询和后端交互的代码

--- xc-ui-pc-sysmanage/src/base/router/index.js
+++ xc-ui-pc-sysmanage/src/base/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
 Vue.use(Router);
 // 定义路由配置
-let routes = []
+let routes = [];
 let concat = (router) => {
   routes = routes.concat(router)
-}
+};
 // // 导入路由规则
 import HomeRouter from '@/module/home/router'
 import CmsRouter from '@/module/cms/router'
