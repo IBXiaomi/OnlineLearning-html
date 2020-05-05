6484d714ccf3ec01dceefba75b3821f43d0515d8
IBXiaomi <13572226807@163.com>
2020-05-05 22:22:58 +0800

    增加页面模板功能

--- xc-ui-pc-sysmanage/src/module/cms/router/index.js
+++ xc-ui-pc-sysmanage/src/module/cms/router/index.js
@@ -2,6 +2,9 @@ import Home from '@/module/home/page/home.vue';
 import page_list from '@/module/cms/page/page_list.vue';
 import page_add from '@/module/cms/page/page_add.vue';
 import page_edit from '@/module/cms/page/page_edit.vue';
+import page_template_list from '@/module/cms/template/page_template_list.vue';
+import page_template_add from '@/module/cms/template/page_template_add.vue';
+import page_template_edit from '@/module/cms/template/page_template_edit.vue';
 
 export default [{
   path: '/',
@@ -11,7 +14,10 @@ export default [{
   children: [
     {path: '/cms/page/list', name: '页面列表', component: page_list, hidden: false},
     {path: '/cms/page/add', name: '新增页面', component: page_add, hidden: true},
-    {path: '/cms/page/edit/:pageId', name: '修改页面', component: page_edit, hidden: true}
+    {path: '/cms/page/edit/:pageId', name: '修改页面', component: page_edit, hidden: true},
+    {path: '/cms/template/list', name: '模板列表', component: page_template_list, hidden: false},
+    {path: '/cms/template/add', name: '新增模板', component: page_template_add, hidden: true},
+    {path: '/cms/template/edit/:pageId', name: '修改模板', component: page_template_edit, hidden: true}
   ]
 }
 ]
--- xc-ui-pc-sysmanage/src/module/cms/template/page_template_list.vue
+++ xc-ui-pc-sysmanage/src/module/cms/template/page_template_list.vue
@@ -2,23 +2,23 @@
   <div>
     <el-form v-model="params">
       <!--编写页面静态部分，即view部分-->
-      <el-select v-model="params.siteId" placeholder="请选择">
+      <el-select v-model="params.templateFileId" placeholder="请选择模板文件id">
         <el-option
-          v-for="item in siteList"
-          :key="item.siteId"
-          :label="item.siteName"
-          :value="item.siteId">
+          v-for="item in list"
+          :key="item.templateId"
+          :label="item.templateFileId"
+          :value="item.templateId">
         </el-option>
       </el-select>
-      页面别名
-      <el-input v-model="params.pageAliase" placeholder="请输入页面别名" style="width:100px"></el-input>
+
+      <el-input v-model="params.templateName" placeholder="请输入模板名称" style="width:150px"></el-input>
       <el-button type="primary" size="medium" v-on:click="query">查询</el-button>
       <router-link :to="{path:'/cms/page/add',query:{
        page:this.params.page,
        siteId:this.params.siteId
 
       }}">
-        <el-button type="primary" size="medium">新增页面</el-button>
+        <el-button type="primary" size="medium">新增模板</el-button>
       </router-link>
     </el-form>
     <el-table
@@ -27,20 +27,20 @@
       style="width: 100%">
       <el-table-column type="index" width="60">
       </el-table-column>
-      <el-table-column prop="pageName" label="页面名称" width="250">
-      </el-table-column>
-      <el-table-column prop="pageAliase" label="别名" width="120">
-      </el-table-column>
-      <el-table-column prop="pageType" label="页面类型" width="150">
-      </el-table-column>
-      <el-table-column prop="pageWebPath" label="访问路径" width="250">
-      </el-table-column>
-      <!--<el-table-column prop="pagePhysicalPath" label="物理路径" width="250">-->
-      <!--</el-table-column>-->
-      <!--<el-table-column prop="pageCreateTime" label="创建时间" width="180">-->
-      <!--</el-table-column>-->
       <el-table-column prop="siteId" label="站点id" width="250">
       </el-table-column>
+      <el-table-column prop="templateId" label="模板id" width="250">
+      </el-table-column>
+      <el-table-column prop="templateName" label="模板名称" width="150">
+      </el-table-column>
+      <el-table-column prop="templateParameter" label="模板参数" width="150">
+      </el-table-column>
+      <el-table-column prop="templateFileId" label="模板文件id" width="250">
+      </el-table-column>
+      <!--<el-table-column prop="pageCreateTime" label="创建时间" width="180">-->
+      <!--</el-table-column>-->
+      <!--<el-table-column prop="siteId" label="站点id" width="180">-->
+      <!--</el-table-column>-->
       <el-table-column label="操作" width="250">
         <template slot-scope="page">
           <el-button
@@ -73,14 +73,13 @@
   export default {
     data() {
       return {
-        siteList: [],
         list: [],
         total: 0,
         params: {
-          page: 1,
+          page: 0,
           size: 10,
-          siteId: '',
-          pageAliase: ''
+          templateFileId: '',
+          templateName: ''
         }
       }
     },
@@ -88,7 +87,9 @@
       query: function () {
         //alert('查询');
         //调用服务端的接口
-        cmsApi.page_list(this.params.page, this.params.size, this.params).then((result) => {
+        // 在向后端传递查询参数时，需要先在return中定义需要传递的参数，否则后端查询数据存在问题
+        cmsApi.page_template_list(this.params.page, this.params.size, this.params).then((result) => {
+
           //将res结果数据赋值给数据模型对象
           this.list = result.queryResult.list;
           this.total = result.queryResult.total;
@@ -125,17 +126,18 @@
         })
       }
     },
-      created() {
-        this.params.page = Number.parseInt(this.$route.query.page || 1),
-          this.params.siteId = this.$route.query.siteId || ''
-      }
-      ,
-      // 实现进入页面显示数据
-      mounted() {
-        this.query();
-        this.siteQuery();
-      }
+
+    // created() {
+    //   this.params.page = Number.parseInt(this.$route.query.page || 1),
+    //     this.params.siteId = this.$route.query.siteId || ''
+    // }
+    // ,
+    // 实现进入页面显示数据
+    mounted() {
+      this.query();
+
     }
+  }
 
 
 </script>
